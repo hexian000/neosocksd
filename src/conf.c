bc71216a66968de85773fa5806b3845a7b6356bc
hexian000 <hexian000@outlook.com>
2023-11-17 21:01:45 +0800

    conf: better checks
    Signed-off-by: hexian000 <hexian000@outlook.com>

--- src/conf.c
+++ src/conf.c
@@ -21,7 +21,7 @@ struct config conf_default(void)
 		.tcp_sndbuf = 0,
 		.tcp_rcvbuf = 0,
 
-		.max_sessions = 4096,
+		.max_sessions = 16384,
 		.startup_limit_start = 10,
 		.startup_limit_rate = 30,
 		.startup_limit_full = 100,
@@ -32,51 +32,74 @@ struct config conf_default(void)
 	return conf;
 }
 
-#define RANGE_CHECK(conf, key, min, max)                                       \
-	do {                                                                   \
-		if ((intmax_t)((conf)->key) < (intmax_t)(min) ||               \
-		    (intmax_t)((conf)->key) > (intmax_t)(max)) {               \
-			LOGE_F("config: %s is out of range (%s - %s)", #key,   \
-			       #min, #max);                                    \
-			return false;                                          \
-		}                                                              \
-	} while (0)
+static bool range_check_size(
+	const char *key, const size_t value, const size_t lbound,
+	const size_t ubound)
+{
+	if (!(lbound <= value && value <= ubound)) {
+		LOGE_F("%s is out of range (%zu - %zu)", key, lbound, ubound);
+		return false;
+	}
+	return true;
+}
 
-#define RANGE_CHECK_FLOAT(conf, key, min, max)                                 \
-	do {                                                                   \
-		if (!(((conf)->key) >= (min) && ((conf)->key) <= (max))) {     \
-			LOGE_F("config: %s is out of range (%s - %s)", #key,   \
-			       #min, #max);                                    \
-			return false;                                          \
-		}                                                              \
-	} while (0)
+static bool range_check_double(
+	const char *key, const double value, const double lbound,
+	const double ubound)
+{
+	if (!(lbound <= value && value <= ubound)) {
+		LOGE_F("%s is out of range (%g - %g)", key, lbound, ubound);
+		return false;
+	}
+	return true;
+}
+
+#define RANGE_CHECK(key, value, lbound, ubound)                                \
+	_Generic(value, size_t                                                 \
+		 : range_check_size, double                                    \
+		 : range_check_double)(key, value, lbound, ubound)
 
 bool conf_check(const struct config *restrict conf)
 {
-	RANGE_CHECK_FLOAT(conf, timeout, 5.0, 86400.0);
-	RANGE_CHECK(conf, startup_limit_start, 1, INT_MAX);
-	RANGE_CHECK(conf, startup_limit_rate, 0, 100);
-	RANGE_CHECK(conf, startup_limit_full, 1, INT_MAX);
-
 	if (conf->listen == NULL) {
-		LOGE("conf: listen address is not specified");
+		LOGE("listen address is not specified");
+		return false;
+	}
+	int proto_flags_sepcified = 0;
+	if (conf->forward != NULL) {
+		proto_flags_sepcified++;
+	}
+	if (conf->http) {
+		proto_flags_sepcified++;
+	}
+#if WITH_TPROXY
+	if (conf->transparent) {
+		proto_flags_sepcified++;
+	}
+#endif
+	if (proto_flags_sepcified > 1) {
+		LOGE("incompatible flags are specified");
 		return false;
 	}
 #if WITH_RULESET
 	if (conf->ruleset != NULL && conf->forward != NULL) {
-		LOGE("conf: specifing ruleset and forward at the same time is ambiguous");
-		return false;
-	}
-#endif
-#if WITH_TPROXY
-	if (conf->transparent && conf->http) {
-		LOGE("tproxy and http cannot be specified at the same time");
-		return false;
+		LOGW("ruleset will not work in forwarding mode");
 	}
 #endif
 	if ((conf->tcp_sndbuf != 0 && conf->tcp_sndbuf < 4096) ||
 	    (conf->tcp_rcvbuf != 0 && conf->tcp_rcvbuf < 4096)) {
-		LOGW("conf: probably too small tcp buffer");
+		LOGW("probably too small tcp buffer");
 	}
-	return true;
+
+	return RANGE_CHECK("timeout", conf->timeout, 5.0, 86400.0) &&
+	       RANGE_CHECK(
+		       "startup_limit_start", conf->startup_limit_start, 1,
+		       conf->startup_limit_full) &&
+	       RANGE_CHECK(
+		       "startup_limit_rate", conf->startup_limit_rate, 0,
+		       100) &&
+	       RANGE_CHECK(
+		       "startup_limit_full", conf->startup_limit_full,
+		       conf->startup_limit_start,
+		       conf->max_sessions > 0 ? conf->max_sessions : SIZE_MAX);
 }
