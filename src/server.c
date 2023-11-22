668b8542f97f33f42dc2da05532f7baa23541bba
hexian000 <hexian000@outlook.com>
2023-11-22 20:45:12 +0800

    debug: tiny fixes
    Signed-off-by: hexian000 <hexian000@outlook.com>

--- src/server.c
+++ src/server.c
@@ -138,7 +138,9 @@ bool server_start(struct server *s, const struct sockaddr *bindaddr)
 		socket_set_transparent(fd, true);
 	}
 #endif
-	const int backlog = MIN(conf->startup_limit_full, SOMAXCONN);
+	const int backlog = (conf->startup_limit_full > 0) ?
+				    conf->startup_limit_full :
+				    SOMAXCONN;
 #if WITH_TCP_FASTOPEN
 	if (conf->tcp_fastopen) {
 		socket_set_fastopen(fd, backlog);
