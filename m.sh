c043bc8663fc833400cb2c9c54bc19c3fe8bb02b
He Xian <hexian000@outlook.com>
2024-10-29 19:03:35 +0800

    build: document m.sh
    Signed-off-by: He Xian <hexian000@outlook.com>

--- m.sh
+++ m.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
+# m.sh: in-tree build script for convenience
 cd "$(dirname "$0")"
 GENERATOR="Unix Makefiles"
+NPROC=1
 set -ex
 
 case "$1" in
+"c")
+    # clean artifacts
+    rm -rf "build" "compile_commands.json"
+    ;;
 "x")
     # cross compiling, environment vars need to be set
     rm -rf "build" && mkdir -p "build"
@@ -13,73 +19,18 @@ case "$1" in
         -DCMAKE_FIND_ROOT_PATH="${SYSROOT};${LIBROOT}" \
         -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
         -S . -B "build"
-    nice cmake --build "build"
-    ls -lh "build/bin/neosocksd"
-    ;;
-"xs")
-    # cross compiling, environment vars need to be set
-    rm -rf "build" && mkdir -p "build"
-    cmake -G "${GENERATOR}" \
-        -DCMAKE_BUILD_TYPE="Release" \
-        -DCMAKE_SYSTEM_NAME="Linux" \
-        -DCMAKE_FIND_ROOT_PATH="${SYSROOT};${LIBROOT}" \
-        -DBUILD_STATIC=ON \
-        -DENABLE_JEMALLOC=ON \
-        -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
-        -S . -B "build"
-    nice cmake --build "build"
-    ls -lh "build/bin/neosocksd"
-    ;;
-"r")
-    rm -rf "build" && mkdir -p "build"
-    cmake -G "${GENERATOR}" \
-        -DCMAKE_BUILD_TYPE="Release" \
-        -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
-        -S . -B "build"
-    nice cmake --build "build"
-    ls -lh "build/bin/neosocksd"
-    ;;
-"s")
-    rm -rf "build" && mkdir -p "build"
-    cmake -G "${GENERATOR}" \
-        -DCMAKE_BUILD_TYPE="Release" \
-        -DBUILD_STATIC=ON \
-        -DENABLE_JEMALLOC=ON \
-        -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
-        -S . -B "build"
-    nice cmake --build "build"
-    ls -lh "build/bin/neosocksd"
-    ;;
-"p")
-    rm -rf "build" && mkdir -p "build"
-    cmake -G "${GENERATOR}" \
-        -DCMAKE_BUILD_TYPE="RelWithDebInfo" \
-        -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
-        -S . -B "build"
-    nice cmake --build "build"
-    (cd "build/bin" && objdump -drwS "neosocksd" >"neosocksd.S")
-    ls -lh "build/bin/neosocksd"
-    ;;
-"min")
-    # rebuild for minimized size
-    rm -rf "build" && mkdir "build"
-    cmake -G "${GENERATOR}" \
-        -DCMAKE_BUILD_TYPE="MinSizeRel" \
-        -DENABLE_RULESET=OFF \
-        -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
-        -S "." -B "build"
-    nice cmake --build "build"
+    nice cmake --build "build" --parallel "${NPROC}"
     ls -lh "build/bin/neosocksd"
     ;;
 "posix")
-    # force POSIX APIs
+    # rebuild for strict POSIX compliance
     rm -rf "build" && mkdir -p "build"
     cmake -G "${GENERATOR}" \
         -DCMAKE_BUILD_TYPE="Release" \
         -DFORCE_POSIX=ON \
         -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
         -S . -B "build"
-    nice cmake --build "build"
+    nice cmake --build "build" --parallel "${NPROC}"
     ls -lh "build/bin/neosocksd"
     ;;
 "clang")
@@ -91,18 +42,19 @@ case "$1" in
         -DCMAKE_EXE_LINKER_FLAGS="-fuse-ld=lld --rtlib=compiler-rt" \
         -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
         -S . -B "build"
-    nice cmake --build "build"
+    nice cmake --build "build" --parallel "${NPROC}"
     (cd "build/bin" && llvm-objdump -drwS "neosocksd" >"neosocksd.S")
     ls -lh "build/bin/neosocksd"
     ;;
 "msys2")
+    # rebuild with MSYS 2
     rm -rf "build" && mkdir "build"
     cmake -G "${GENERATOR}" \
         -DCMAKE_BUILD_TYPE="Release" \
         -DENABLE_LTO=OFF \
         -DCMAKE_EXE_LINKER_FLAGS="-static-libgcc" \
         -S "." -B "build"
-    nice cmake --build "build"
+    nice cmake --build "build" --parallel "${NPROC}"
     HOST="$(cc -dumpmachine)"
     zip -9j "build/neosocksd-win32.${HOST}.zip" \
         "/usr/bin/msys-2.0.dll" \
@@ -111,32 +63,18 @@ case "$1" in
     ls -lh "build/neosocksd-win32.${HOST}.zip"
     ;;
 "ndk")
-    # cross compiling, environment vars need to be set
+    # rebuild with Android NDK
     rm -rf "build" && mkdir "build"
     cmake -G "${GENERATOR}" \
         -DCMAKE_BUILD_TYPE="Release" \
-        -DCMAKE_ANDROID_NDK="${NDK}" \
+        -DCMAKE_ANDROID_NDK="${ANDROID_NDK_ROOT}" \
         -DCMAKE_SYSTEM_NAME="Android" \
-        -DCMAKE_SYSTEM_VERSION="${API}" \
-        -DCMAKE_ANDROID_ARCH_ABI="${ABI}" \
+        -DCMAKE_SYSTEM_VERSION="${ANDROID_API_LEVEL}" \
+        -DCMAKE_ANDROID_ARCH_ABI="${ABI_NAME}" \
         -DCMAKE_FIND_ROOT_PATH="${SYSROOT};${LIBROOT}" \
         -DLINK_STATIC_LIBS=ON \
         -S "." -B "build"
-    nice cmake --build "build"
-    ls -lh "build/bin/neosocksd"
-    ;;
-"d")
-    if command -v clang-format >/dev/null; then
-        find src -type f -regex '.*\.[hc]' -exec clang-format -i {} +
-    fi
-    # debug
-    rm -rf "build" && mkdir -p "build"
-    cmake -G "${GENERATOR}" \
-        -DCMAKE_BUILD_TYPE="Debug" \
-        -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
-        -S . -B "build"
-    ln -sf build/compile_commands.json compile_commands.json
-    nice cmake --build "build" --parallel
+    nice cmake --build "build" --parallel "${NPROC}"
     ls -lh "build/bin/neosocksd"
     ;;
 "san")
@@ -151,8 +89,51 @@ case "$1" in
     nice cmake --build "build" --parallel
     ls -lh "build/bin/neosocksd"
     ;;
-"c")
-    rm -rf "build" "compile_commands.json"
+"min")
+    # rebuild for minimized size
+    rm -rf "build" && mkdir "build"
+    cmake -G "${GENERATOR}" \
+        -DCMAKE_BUILD_TYPE="MinSizeRel" \
+        -DENABLE_RULESET=OFF \
+        -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+        -S "." -B "build"
+    nice cmake --build "build" --parallel "${NPROC}"
+    ls -lh "build/bin/neosocksd"
+    ;;
+"p")
+    # rebuild for profiling
+    rm -rf "build" && mkdir -p "build"
+    cmake -G "${GENERATOR}" \
+        -DCMAKE_BUILD_TYPE="RelWithDebInfo" \
+        -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+        -S . -B "build"
+    nice cmake --build "build" --parallel "${NPROC}"
+    (cd "build/bin" && objdump -drwS "neosocksd" >"neosocksd.S")
+    ls -lh "build/bin/neosocksd"
+    ;;
+"r")
+    # rebuild for release
+    rm -rf "build" && mkdir -p "build"
+    cmake -G "${GENERATOR}" \
+        -DCMAKE_BUILD_TYPE="Release" \
+        -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+        -S . -B "build"
+    nice cmake --build "build" --parallel "${NPROC}"
+    ls -lh "build/bin/neosocksd"
+    ;;
+"d")
+    # rebuild for debug
+    if command -v clang-format >/dev/null; then
+        find src -type f -regex '.*\.[hc]' -exec clang-format -i {} +
+    fi
+    rm -rf "build" && mkdir -p "build"
+    cmake -G "${GENERATOR}" \
+        -DCMAKE_BUILD_TYPE="Debug" \
+        -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+        -S . -B "build"
+    ln -sf build/compile_commands.json compile_commands.json
+    nice cmake --build "build" --parallel
+    ls -lh "build/bin/neosocksd"
     ;;
 *)
     nice cmake --build "build" --parallel
