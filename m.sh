c18d8e3dfca685f3d5a5a8477e74a720b658c028
He Xian <hexian000@outlook.com>
2024-11-02 10:37:00 +0800

    build: bump cmake to 3.13 for interprocedural optimization
    Signed-off-by: He Xian <hexian000@outlook.com>

--- m.sh
+++ m.sh
@@ -17,7 +17,7 @@ case "$1" in
         -DCMAKE_BUILD_TYPE="Release" \
         -DCMAKE_SYSTEM_NAME="Linux" \
         -DCMAKE_FIND_ROOT_PATH="${SYSROOT};${LIBROOT}" \
-        -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+        -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON \
         -S . -B "build"
     nice cmake --build "build" --parallel "${NPROC}"
     ls -lh "build/bin/neosocksd"
@@ -26,9 +26,10 @@ case "$1" in
     # rebuild for strict POSIX compliance
     rm -rf "build" && mkdir -p "build"
     cmake -G "${GENERATOR}" \
+        -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
         -DCMAKE_BUILD_TYPE="Release" \
         -DFORCE_POSIX=ON \
-        -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+        -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON \
         -S . -B "build"
     nice cmake --build "build" --parallel "${NPROC}"
     ls -lh "build/bin/neosocksd"
@@ -37,10 +38,11 @@ case "$1" in
     # rebuild with Linux clang/lld
     rm -rf "build" && mkdir -p "build"
     cmake -G "${GENERATOR}" \
+        -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
         -DCMAKE_BUILD_TYPE="RelWithDebInfo" \
         -DCMAKE_C_COMPILER="clang" \
         -DCMAKE_EXE_LINKER_FLAGS="-fuse-ld=lld --rtlib=compiler-rt" \
-        -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+        -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON \
         -S . -B "build"
     nice cmake --build "build" --parallel "${NPROC}"
     (cd "build/bin" && llvm-objdump -drwS "neosocksd" >"neosocksd.S")
@@ -51,7 +53,7 @@ case "$1" in
     rm -rf "build" && mkdir "build"
     cmake -G "${GENERATOR}" \
         -DCMAKE_BUILD_TYPE="Release" \
-        -DENABLE_LTO=OFF \
+        -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON \
         -DCMAKE_EXE_LINKER_FLAGS="-static-libgcc" \
         -S "." -B "build"
     nice cmake --build "build" --parallel "${NPROC}"
@@ -72,6 +74,7 @@ case "$1" in
         -DCMAKE_SYSTEM_VERSION="${ANDROID_API_LEVEL}" \
         -DCMAKE_ANDROID_ARCH_ABI="${ABI_NAME}" \
         -DCMAKE_FIND_ROOT_PATH="${SYSROOT};${LIBROOT}" \
+        -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON \
         -DENABLE_MIMALLOC=ON \
         -DLINK_STATIC_LIBS=ON \
         -S "." -B "build"
@@ -82,10 +85,10 @@ case "$1" in
     # rebuild with clang & sanitizers
     rm -rf "build" && mkdir -p "build"
     cmake -G "${GENERATOR}" \
+        -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
         -DCMAKE_BUILD_TYPE="Debug" \
         -DCMAKE_C_COMPILER="clang" \
         -DENABLE_SANITIZERS=ON \
-        -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
         -S . -B "build"
     nice cmake --build "build" --parallel
     ls -lh "build/bin/neosocksd"
@@ -94,9 +97,10 @@ case "$1" in
     # rebuild for minimized size
     rm -rf "build" && mkdir "build"
     cmake -G "${GENERATOR}" \
-        -DCMAKE_BUILD_TYPE="MinSizeRel" \
-        -DENABLE_RULESET=OFF \
         -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+        -DCMAKE_BUILD_TYPE="MinSizeRel" \
+        -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON \
+        -DENABLE_RULESET=OFF \
         -S "." -B "build"
     nice cmake --build "build" --parallel "${NPROC}"
     ls -lh "build/bin/neosocksd"
@@ -105,8 +109,9 @@ case "$1" in
     # rebuild for profiling
     rm -rf "build" && mkdir -p "build"
     cmake -G "${GENERATOR}" \
-        -DCMAKE_BUILD_TYPE="RelWithDebInfo" \
         -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+        -DCMAKE_BUILD_TYPE="RelWithDebInfo" \
+        -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON \
         -S . -B "build"
     nice cmake --build "build" --parallel "${NPROC}"
     (cd "build/bin" && objdump -drwS "neosocksd" >"neosocksd.S")
@@ -116,8 +121,9 @@ case "$1" in
     # rebuild for release
     rm -rf "build" && mkdir -p "build"
     cmake -G "${GENERATOR}" \
-        -DCMAKE_BUILD_TYPE="Release" \
         -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+        -DCMAKE_BUILD_TYPE="Release" \
+        -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON \
         -S . -B "build"
     nice cmake --build "build" --parallel "${NPROC}"
     ls -lh "build/bin/neosocksd"
